fbe772db1400053d1e866f2e33f5bbf0e1c8a6ef
Davide-Console <davideconsole99@gmail.com>
2022-11-17 18:15:37 +0100

    shooting star naa nana naa nana

--- Space_ADHD/Assets/ScriptsMenu/shootingStarEffect.cs
+++ Space_ADHD/Assets/ScriptsMenu/shootingStarEffect.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class shootingStarEffect : MonoBehaviour
 {
     public GameObject shootingStar;
-    public Vector3 targetPosition = new Vector3(0, 0, 0);   
+    public Vector3 targetPosition = new Vector3(-10000, -10000, -10000);   
     // Start is called before the first frame update
-    private float speed = 0.2f;
+    private float speed = 0.02f;
     void Start()
     {
         shootingStar = GameObject.Find("shootingStar");
+        targetPosition = new Vector3(-10000, -10000, -10000);
     }
     // Update is called once per frame
     void Update()
@@ -18,19 +19,19 @@ public class shootingStarEffect : MonoBehaviour
         var step = speed;
         //public GameObject shootingStar;
 
-        if (Time.frameCount % 1000 == 0)
+        if (Time.frameCount % 500 == 0)
         {
             //shootingStar.GetComponent<Rigidbody>().velocity = Vector3.zero;
             Debug.Log(Time.timeAsDouble);
-            float spawnY = Random.Range(-6.0f, 6.0f);
-            float targetY = Random.Range(-30.0f, 30.0f);
-            float spawnX = Random.Range(-4.0f, 4.0f);
-            float targetX = Random.Range(-20.0f, 20.0f);
+            float spawnY = Random.Range(-7.5f, 9.5f) * (Random.Range(0, 2) * 2 - 1);
+            float targetY = - spawnY + Random.Range(-7.0f, 7.0f);
+            float spawnX = Random.Range(-5.0f, 7.0f) * (Random.Range(0, 2) * 2 - 1);
+            float targetX = - spawnX + Random.Range(-7.0f, 7.0f);
             float spawnZ = Random.Range(0.0f, 2.0f);
-            float targetZ = Random.Range(0.0f, 2.0f);
+            float targetZ = Random.Range(-30.0f, -3.0f);
             Vector3 spawnPosition = new Vector3(spawnX, spawnY, spawnZ);
             targetPosition = new Vector3(targetX, targetY, targetZ);
-
+            shootingStar.GetComponent<Rigidbody>().velocity = Vector3.zero;
             shootingStar.transform.position = spawnPosition;
             Debug.Log(spawnPosition);
             Debug.Log(targetPosition);
