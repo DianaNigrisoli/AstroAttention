1b0b55ed87b1240303f3e2de6b4c67b295654762
RaffaeleBerzoini <raffaele.berzoini@mail.polimi.it>
2022-11-20 13:16:32 +0100

    project cleaning

---
+++ Space_ADHD/Assets/ScriptsDirectionGame/upRightButton.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class upRightButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = new Vector3(Screen.width / 2, Screen.height / 6, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
