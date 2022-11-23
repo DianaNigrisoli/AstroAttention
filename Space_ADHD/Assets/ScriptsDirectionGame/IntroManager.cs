a231c7f7128fd03efc8149203d4fea00c5b778b7
RaffaeleBerzoini <raffaele.berzoini@mail.polimi.it>
2022-11-23 21:48:41 +0100

    Welcome Help robot

--- Space_ADHD/Assets/ScriptsDirectionGame/IntroManager.cs
+++ Space_ADHD/Assets/ScriptsDirectionGame/IntroManager.cs
@@ -9,9 +9,11 @@ public class IntroManager : MonoBehaviour
 {
     [SerializeField] private GameObject tutorialCanvas;
     [SerializeField] private TextMeshProUGUI tutorialText;
+    [SerializeField] private GameObject tutorialRobot;
 
     private float tmpTutorialTime = 5.0f;
     private Boolean showingTutorial;
+    
     void Awake()
     {
         MiniGameManager.OnMiniGameStateChanged += MiniGameManagerOnOnMiniGameStateChanged;
@@ -30,6 +32,11 @@ public class IntroManager : MonoBehaviour
             tutorialCanvas.SetActive(true);
             tutorialText.transform.position = new Vector3(Screen.width / 2.0f, Screen.height / 1.30f, 0);
             tutorialText.text = "Tutorial...";
+            
+            tutorialRobot = Instantiate(tutorialRobot, GameObject.Find("All").transform);
+            tutorialRobot.transform.Rotate(-3.611f, -154.285f, 0);
+            tutorialRobot.transform.position = new Vector3(0.1408024f, 1.262115f, 0.7617905f);
+            
             showingTutorial = true;
         }
     }
@@ -44,6 +51,7 @@ public class IntroManager : MonoBehaviour
             {
                 tutorialCanvas.SetActive(false);
                 MiniGameManager.instance.UpdateMiniGameState(MiniGameState.WaitForNext);
+                Destroy(tutorialRobot);
                 Destroy(this);
             }
         }
