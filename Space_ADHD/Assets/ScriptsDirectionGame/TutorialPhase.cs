46fe6cd9ffeb0db4fed4391694ea88f4a3d0dfdc
RaffaeleBerzoini <raffaele.berzoini@mail.polimi.it>
2022-11-25 18:20:05 +0100

    Davi infame capo dei mergiatori

---
+++ Space_ADHD/Assets/ScriptsDirectionGame/TutorialPhase.cs
@@ -0,0 +1,17 @@
+namespace Assets.ScriptsDirectionGame
+{
+    public enum TutorialPhase
+    {
+        Zero,
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten
+    }
+}
