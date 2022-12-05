6407591c7a51ab998153223023e43752f480aed2
Davide-Console <davideconsole99@gmail.com>
2022-12-05 17:33:19 +0100

    shot positions

--- Space_ADHD/Assets/ScriptsDirectionGame/CannonBehavior.cs
+++ Space_ADHD/Assets/ScriptsDirectionGame/CannonBehavior.cs
@@ -2,111 +2,123 @@
 using UnityEngine;
 using System.Collections;
 using Assets.ScriptsDirectionGame;
+using Assets.Scripts_A_General;
 
-public class CannonBehavior : MonoBehaviour {
-
-	public GameObject m_shotPrefab;
-	public static Boolean upRightShot;
-	public static Boolean upLeftShot;
-	public static Boolean downRightShot;
-	public static Boolean downLeftShot;
-	public GameObject target;
-    public GameObject shootingStar;
-    public static double kidScoreDirectionG = 0.0d;
-
-	void Start()
-	{
-		upRightShot = false;
-		upLeftShot = false;
-		downLeftShot = false;
-		downRightShot = false;
-		shootingStar = GameObject.Find("smallShootingStar");
-	}
-
-	// Update is called once per frame
-	void Update ()
-	{
-		if (phase0Manager.phaseZero || phase1Manager.phaseOne)
-		{
-			if (upRightShot)
-			{
-				Shoot(Vector3.forward * 5f + Vector3.up * 0.99f + Vector3.right * 0.7f);
-				upRightShot = false;
-				kidScoreDirectionG = kidScoreDirectionG + (1.0f * (5-laserButtons.reactionTime));
-				//Debug.Log(kidScoreDirectionG);
-			}
-			if (upLeftShot)
-			{
-				Shoot(Vector3.forward * 5f + Vector3.up * 0.99f + Vector3.left * 0.7f);
-				upLeftShot = false;
-				kidScoreDirectionG = kidScoreDirectionG + (1.0f * (5-laserButtons.reactionTime));
-				//Debug.Log(kidScoreDirectionG);
-			}
-			if (downLeftShot)
-			{
-				Shoot(Vector3.forward * 5f + Vector3.down * 0.5f + Vector3.left * 0.7f);
-				downLeftShot = false;
-				kidScoreDirectionG = kidScoreDirectionG + (1.0f * (5-laserButtons.reactionTime));
-				//Debug.Log(kidScoreDirectionG);
-			}
-			if (downRightShot)
-			{
-				Shoot(Vector3.forward * 5f + Vector3.down * 0.5f + Vector3.right * 0.7f);
-				downRightShot = false;
-				kidScoreDirectionG = kidScoreDirectionG + (1.0f * (5-laserButtons.reactionTime));
-				//Debug.Log(kidScoreDirectionG);
-			}
-		}
-		else if (phase2Manager.phaseTwo || phase3Manager.phaseThree)
-		{
-			if (shootingStar.transform. position == new Vector3(2.25f, 4.7f, 16.4f) && (upRightShot || upLeftShot || downRightShot || downLeftShot))
-			{
-				Shoot(Vector3.forward * 5f + Vector3.up * 0.99f + Vector3.right * 0.7f);
-				upRightShot = false;
-				upLeftShot = false;
-				downLeftShot = false;
-				downRightShot = false;
-				kidScoreDirectionG = kidScoreDirectionG + (2.0f * (5-laserButtons.reactionTime));
-				//Debug.Log(kidScoreDirectionG);
-			}
-			if (shootingStar.transform. position == new Vector3(-2.25f, 4.7f, 16.4f) && (upRightShot || upLeftShot || downRightShot || downLeftShot))
-			{
-				Shoot(Vector3.forward * 5f + Vector3.up * 0.99f + Vector3.left * 0.7f);
-				upRightShot = false;
-				upLeftShot = false;
-				downLeftShot = false;
-				downRightShot = false;
-				kidScoreDirectionG = kidScoreDirectionG + (2.0f * (5-laserButtons.reactionTime));
-				//Debug.Log(kidScoreDirectionG);
-			}
-			if (shootingStar.transform. position == new Vector3(-2.25f, 0.45f, 16.4f) && (upRightShot || upLeftShot || downRightShot || downLeftShot))
-			{
-				Shoot(Vector3.forward * 5f + Vector3.down * 0.5f + Vector3.left * 0.7f);
-				upRightShot = false;
-				upLeftShot = false;
-				downLeftShot = false;
-				downRightShot = false;
-				kidScoreDirectionG = kidScoreDirectionG + (2.0f * (5-laserButtons.reactionTime));
-				//Debug.Log(kidScoreDirectionG);
-			}
-			if (shootingStar.transform. position == new Vector3(2.25f, 0.45f, 16.4f) && (upRightShot || upLeftShot || downRightShot || downLeftShot))
-			{
-				Shoot(Vector3.forward * 5f + Vector3.down * 0.5f + Vector3.right * 0.7f);
-				upRightShot = false;
-				upLeftShot = false;
-				downLeftShot = false;
-				downRightShot = false;
-				kidScoreDirectionG = kidScoreDirectionG + (2.0f * (5-laserButtons.reactionTime));
-				//Debug.Log(kidScoreDirectionG);
-			}
-		}
-	}
-
-	private void Shoot(Vector3 direction)
-	{
-		laserButtons.enabled = false;
-		GameObject go = GameObject.Instantiate(m_shotPrefab, gameObject.transform.position, gameObject.transform.rotation) as GameObject;
-		go.GetComponent<Rigidbody>().velocity = direction * 50.0f;
-		GameObject.Destroy(go, 1.5f);
-	}
+namespace Assets.ScriptsDirectionGame
+{
+    public class CannonBehavior : MonoBehaviour {
+    
+    	public GameObject m_shotPrefab;
+    	public static Boolean upRightShot;
+    	public static Boolean upLeftShot;
+    	public static Boolean downRightShot;
+    	public static Boolean downLeftShot;
+    	public GameObject target;
+        public GameObject shootingStar;
+        public static double kidScoreDirectionG = 0.0d;
+        private MiniGameState miniGameState;
+    
+    	void Start()
+    	{
+    		upRightShot = false;
+    		upLeftShot = false;
+    		downLeftShot = false;
+    		downRightShot = false;
+    		shootingStar = GameObject.Find("smallShootingStar");
+    	}
+    
+        void Awake()
+        {
+            MiniGameManager.OnMiniGameStateChanged += MiniGameManagerOnOnMiniGameStateChanged;
+        }
+    
+        void OnDestroy()
+        {
+            MiniGameManager.OnMiniGameStateChanged -= MiniGameManagerOnOnMiniGameStateChanged;
+        }
+        
+        private void MiniGameManagerOnOnMiniGameStateChanged(MiniGameState newState)
+        {
+            miniGameState = newState;
+        }
+            
+    	// Update is called once per frame
+    	void Update ()
+    	{
+    		if (miniGameState == MiniGameState.Zero || miniGameState == MiniGameState.One)
+    		{
+    			if (upRightShot)
+    			{
+    				Shoot(Vector3.forward * 5f + Vector3.up * 0.99f + Vector3.right * 0.7f);
+    				upRightShot = false;
+    				kidScoreDirectionG = kidScoreDirectionG + (1.0f * (5-laserButtons.reactionTime)) + 1;
+    			}
+    			if (upLeftShot)
+    			{
+    				Shoot(Vector3.forward * 5f + Vector3.up * 0.99f + Vector3.left * 0.7f);
+    				upLeftShot = false;
+    				kidScoreDirectionG = kidScoreDirectionG + (1.0f * (5-laserButtons.reactionTime)) + 1;
+    			}
+    			if (downLeftShot)
+    			{
+    				Shoot(Vector3.forward * 5f + Vector3.down * 0.4f + Vector3.left * 0.7f);
+    				downLeftShot = false;
+    				kidScoreDirectionG = kidScoreDirectionG + (1.0f * (5-laserButtons.reactionTime)) + 1;
+    			}
+    			if (downRightShot)
+    			{
+    				Shoot(Vector3.forward * 5f + Vector3.down * 0.4f + Vector3.right * 0.7f);
+    				downRightShot = false;
+    				kidScoreDirectionG = kidScoreDirectionG + (1.0f * (5-laserButtons.reactionTime)) + 1;
+    			}
+    		}
+    		else if (miniGameState == MiniGameState.Two || miniGameState == MiniGameState.Three)
+    		{
+    			if (shootingStar.transform. position == new Vector3(2.25f, 4.7f, 16.4f) && (upRightShot || upLeftShot || downRightShot || downLeftShot))
+    			{
+    				Shoot(Vector3.forward * 5f + Vector3.up * 0.99f + Vector3.right * 0.7f);
+    				upRightShot = false;
+    				upLeftShot = false;
+    				downLeftShot = false;
+    				downRightShot = false;
+    				kidScoreDirectionG = kidScoreDirectionG + (2.0f * (5-laserButtons.reactionTime)) + 1;
+    			}
+    			if (shootingStar.transform. position == new Vector3(-2.25f, 4.7f, 16.4f) && (upRightShot || upLeftShot || downRightShot || downLeftShot))
+    			{
+    				Shoot(Vector3.forward * 5f + Vector3.up * 0.99f + Vector3.left * 0.7f);
+    				upRightShot = false;
+    				upLeftShot = false;
+    				downLeftShot = false;
+    				downRightShot = false;
+    				kidScoreDirectionG = kidScoreDirectionG + (2.0f * (5-laserButtons.reactionTime)) + 1;
+    			}
+    			if (shootingStar.transform. position == new Vector3(-2.25f, 0.45f, 16.4f) && (upRightShot || upLeftShot || downRightShot || downLeftShot))
+    			{
+    				Shoot(Vector3.forward * 5f + Vector3.down * 0.5f + Vector3.left * 0.7f);
+    				upRightShot = false;
+    				upLeftShot = false;
+    				downLeftShot = false;
+    				downRightShot = false;
+    				kidScoreDirectionG = kidScoreDirectionG + (2.0f * (5-laserButtons.reactionTime)) + 1;
+    			}
+    			if (shootingStar.transform. position == new Vector3(2.25f, 0.45f, 16.4f) && (upRightShot || upLeftShot || downRightShot || downLeftShot))
+    			{
+    				Shoot(Vector3.forward * 5f + Vector3.down * 0.5f + Vector3.right * 0.7f);
+    				upRightShot = false;
+    				upLeftShot = false;
+    				downLeftShot = false;
+    				downRightShot = false;
+    				kidScoreDirectionG = kidScoreDirectionG + (2.0f * (5-laserButtons.reactionTime)) + 1;
+    			}
+    		}
+    	}
+    
+    	private void Shoot(Vector3 direction)
+    	{
+    		laserButtons.enabled = false;
+    		GameObject go = GameObject.Instantiate(m_shotPrefab, gameObject.transform.position, gameObject.transform.rotation) as GameObject;
+    		go.GetComponent<Rigidbody>().velocity = direction * 50.0f;
+    		GameObject.Destroy(go, 1.5f);
+    	}
+    }
 }
--- Space_ADHD/Assets/ScriptsDirectionGame/laserButtons.cs
+++ Space_ADHD/Assets/ScriptsDirectionGame/laserButtons.cs
@@ -12,34 +12,24 @@ namespace Assets.ScriptsDirectionGame
         private bool selected; //TODO: do we need this?
         private AudioSource audioData;
         public GameObject buzzButton;
-        private MiniGameState miniGameState; //TODO: useless since I added tutorialPhase... But can be more clear to
-                                             //add that we are in the intro phase in my opinion. Davi if you agree
-                                             //delete this comment;
         private TutorialPhase tutorialPhase;
         public static Boolean enabled;
         public static float FinalScore;
 		public static double reactionTime=5.0d;
-		public static double[] MAreactionTime = new double[] {0, 0, 1, 0, 0};
+		public static double[] reactionTimeMeanStd = new double[] {0, 0, 1, 0, 0};
 		public static float timeDG;
 		public static int errorDirectionG = 0;
         
         void Awake()
         {
-            MiniGameManager.OnMiniGameStateChanged += MiniGameManagerOnOnMiniGameStateChanged;
             IntroManager.OnTutorialPhaseChanged += IntroManagerOnOnTutorialPhaseChanged;
         }
 
         void OnDestroy()
         {
-            MiniGameManager.OnMiniGameStateChanged -= MiniGameManagerOnOnMiniGameStateChanged;
             IntroManager.OnTutorialPhaseChanged += IntroManagerOnOnTutorialPhaseChanged;
         }
-        
-        private void MiniGameManagerOnOnMiniGameStateChanged(MiniGameState newState)
-        {
-            miniGameState = newState;
-        }
-        
+                
         private void IntroManagerOnOnTutorialPhaseChanged(TutorialPhase newPhase)
         {
             tutorialPhase = newPhase;
@@ -54,7 +44,7 @@ namespace Assets.ScriptsDirectionGame
             audioData = button.GetComponent<AudioSource>();
         }
         
-		double[] computeMovingAverage(double newValue, double oldValue, double count, double s1, double s2)
+		double[] computeMeanStd(double newValue, double oldValue, double count, double s1, double s2)
 		{
 			double[] output = new double[5]; //[s1, s2, count, mean, std]
 			output[0] = s1 + newValue;
@@ -78,13 +68,13 @@ namespace Assets.ScriptsDirectionGame
                     {
                         case "UpperRightButtonQuad":
                             if (phase0Manager.cases == 2 || phase1Manager.cases == 1 || (phase2Manager.ROTcases == 1) ||
-                                (phase3Manager.SPTcases == 2) || (miniGameState == MiniGameState.Intro && tutorialPhase == TutorialPhase.Seven))
+                                (phase3Manager.SPTcases == 2) || (tutorialPhase == TutorialPhase.Seven))
                             {
 								if (tutorialPhase == TutorialPhase.Seven){reactionTime = 5.0d;}
 								else
 								{
 									reactionTime = (double)timeDG;
-									MAreactionTime = computeMovingAverage(timeDG, MAreactionTime[3], MAreactionTime[2], MAreactionTime[0], MAreactionTime[1]);
+									reactionTimeMeanStd = computeMeanStd(timeDG, reactionTimeMeanStd[3], reactionTimeMeanStd[2], reactionTimeMeanStd[0], reactionTimeMeanStd[1]);
 								}
 								if (reactionTime>5){reactionTime=5;}
                                 CannonBehavior.upRightShot = true;
@@ -102,13 +92,14 @@ namespace Assets.ScriptsDirectionGame
                             }
                             break;
                         case "UpperLeftButtonQuad":
-                            if (phase0Manager.cases==0 || phase1Manager.cases == 3 || (phase2Manager.ROTcases == 3) || (phase3Manager.SPTcases == 0) || (miniGameState == MiniGameState.Intro && tutorialPhase == TutorialPhase.Five))
+                            if (phase0Manager.cases==0 || phase1Manager.cases == 3 || (phase2Manager.ROTcases == 3) ||
+								 (phase3Manager.SPTcases == 0) || (tutorialPhase == TutorialPhase.Five))
                             {
 								if (tutorialPhase == TutorialPhase.Five){reactionTime = 5.0d;}
 								else
 								{
 									reactionTime = (double)timeDG;
-									MAreactionTime = computeMovingAverage(timeDG, MAreactionTime[3], MAreactionTime[2], MAreactionTime[0], MAreactionTime[1]);
+									reactionTimeMeanStd = computeMeanStd(timeDG, reactionTimeMeanStd[3], reactionTimeMeanStd[2], reactionTimeMeanStd[0], reactionTimeMeanStd[1]);
 								}						
 								if (reactionTime>5){reactionTime=5;}
                                 CannonBehavior.upLeftShot = true;
@@ -129,7 +120,7 @@ namespace Assets.ScriptsDirectionGame
                             if (phase0Manager.cases == 3 || phase1Manager.cases == 0 || (phase2Manager.ROTcases == 0) || (phase3Manager.SPTcases == 3))
                             {
 								reactionTime = (double)timeDG;
-								MAreactionTime = computeMovingAverage(timeDG, MAreactionTime[3], MAreactionTime[2], MAreactionTime[0], MAreactionTime[1]);
+								reactionTimeMeanStd = computeMeanStd(timeDG, reactionTimeMeanStd[3], reactionTimeMeanStd[2], reactionTimeMeanStd[0], reactionTimeMeanStd[1]);
 								if (reactionTime>5){reactionTime=5;}
                                 CannonBehavior.downRightShot = true;
                                 phase0Manager.touch = true;
@@ -148,7 +139,7 @@ namespace Assets.ScriptsDirectionGame
                             if (phase0Manager.cases == 1 || phase1Manager.cases == 2 || (phase2Manager.ROTcases == 2) || (phase3Manager.SPTcases == 1))
                             {
 								reactionTime = (double)timeDG;
-								MAreactionTime = computeMovingAverage(timeDG, MAreactionTime[3], MAreactionTime[2], MAreactionTime[0], MAreactionTime[1]);
+								reactionTimeMeanStd = computeMeanStd(timeDG, reactionTimeMeanStd[3], reactionTimeMeanStd[2], reactionTimeMeanStd[0], reactionTimeMeanStd[1]);
 								if (reactionTime>5){reactionTime=5;}
                                 CannonBehavior.downLeftShot = true;
                                 phase0Manager.touch = true;
