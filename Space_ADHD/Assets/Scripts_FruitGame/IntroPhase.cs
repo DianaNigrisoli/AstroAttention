30190824b7c1e5fcb1ec3a7cf80b6b9487a8a6fc
Diana Nigrisoli <diana.nigrisoli@mail.polimi.it>
2022-12-05 19:38:35 +0100

    start Intro Fruit coding (the script is not active, it's commented

---
+++ Space_ADHD/Assets/Scripts_FruitGame/IntroPhase.cs
@@ -0,0 +1,17 @@
+namespace Assets.Scripts_FruitGame
+{
+    public enum IntroPhase
+    {
+        Zero,
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten
+    }
+}
