eea673d99c6532223d182d938c86b521761252c1
Diana Nigrisoli <diana.nigrisoli@mail.polimi.it>
2022-12-11 15:27:18 +0100

    background music added fruit

---
+++ Space_ADHD/Assets/Scripts_FruitGame/BackgroundMusicManager.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts_FruitGame;
+using UnityEngine;
+
+public class BackgroundMusicManager : MonoBehaviour
+{
+    private AudioSource sound; 
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        MiniGameManagerFruit.OnMiniGameStateChanged += MiniGameManagerOnOnMiniGameStateChanged;
+    }
+
+    void OnDestroy()
+    {
+        MiniGameManagerFruit.OnMiniGameStateChanged -= MiniGameManagerOnOnMiniGameStateChanged;
+    }
+
+    private void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
+
+    private void MiniGameManagerOnOnMiniGameStateChanged(MiniGameStateFruit state)
+    {
+        if (state == MiniGameStateFruit.Intro || state == MiniGameStateFruit.Instructions || state == MiniGameStateFruit.WaitForNext)
+            return;
+        else
+        {
+            sound.Play();
+        }
+    }
+}
+
