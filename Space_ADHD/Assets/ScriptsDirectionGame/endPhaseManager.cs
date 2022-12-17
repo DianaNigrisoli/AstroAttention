842799add92f5249f6664ae9f9985d65ef4ed5fe
LauPozzi <laura3.pozzi@mail.polimi.it>
2022-12-17 17:43:04 +0100

    endphase question traslated direction

--- Space_ADHD/Assets/ScriptsDirectionGame/endPhaseManager.cs
+++ Space_ADHD/Assets/ScriptsDirectionGame/endPhaseManager.cs
@@ -16,6 +16,17 @@ using Assets.ScriptsDirectionGame;
         [SerializeField] GameObject vLasers;
         [SerializeField] GameObject cockpit;
         private MiniGameState lastPlayedPhase;
+        
+        [SerializeField] TextMeshProUGUI score_title;
+        [SerializeField] TextMeshProUGUI question1;
+        [SerializeField] TextMeshProUGUI question2;
+        private string ing_score_t = "SCORE";
+        private string ing_text_q1 = "If I had this game on my iPad, I think I would like to play it a lot";
+        private string ing_text_q2 = "I was proud of how I played";   
+   
+        private string ita_score_t = "PUNTEGGIO";
+        private string ita_text_q1 = "Se avessi questo gioco sul mio iPad, penso mi piacerebbe giocarci molto"; 
+        private string ita_text_q2 = "Sono entusiasta di come ho giocato"; 
 
         void Awake()
         {
@@ -33,6 +44,18 @@ using Assets.ScriptsDirectionGame;
             if (state == MiniGameState.End)
             {
                 endCanvas.SetActive(true);
+                if (GameManager.instance.Language == "ENG")
+                {
+	                score_title.text = ing_score_t;
+	                question1.text = ing_text_q1;
+	                question2.text = ing_text_q2;
+                }
+                else
+                {
+	                score_title.text = ita_score_t;
+	                question1.text = ita_text_q1;
+	                question2.text = ita_text_q2;
+                }
 	            introCanvas.SetActive(false);
     	        wfnCanvas.SetActive(false);
 				string score = string.Format("{0:N2}", CannonBehavior.kidScoreDirectionG);
