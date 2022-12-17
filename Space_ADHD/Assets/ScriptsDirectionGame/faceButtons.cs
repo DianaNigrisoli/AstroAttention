e3d5f1c212fb3043e33310f60f1a3ca89c4e993d
noemimanzo <noemi.manzo@mail.polimi.it>
2022-12-17 13:33:07 +0100

    adj more.png and face buttons

--- Space_ADHD/Assets/ScriptsDirectionGame/faceButtons.cs
+++ Space_ADHD/Assets/ScriptsDirectionGame/faceButtons.cs
@@ -22,6 +22,9 @@ public class faceButtons : MonoBehaviour
         image3 = this.GetComponent<RawImage>();
         image1 = otherButton1.GetComponent<RawImage>();
         image2 = otherButton2.GetComponent<RawImage>();
+        image3.color = new Color(image3.color.r, image3.color.g, image3.color.b, 0.5f);
+        image1.color = new Color(image1.color.r, image1.color.g, image1.color.b, 0.5f);
+        image2.color = new Color(image2.color.r, image2.color.g, image2.color.b, 0.5f);
         button.onClick.AddListener(TaskOnClick);
     }
 
